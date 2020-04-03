2a269d82c4a8d6f8b464f08360dc227e975267ad
shaked1609 <36158504+shaked1609@users.noreply.github.com>
2020-04-03 04:17:36 +0300

    beta1.2

--- app/src/main/java/com/example/beta11/Main2Activity.java
+++ app/src/main/java/com/example/beta11/Main2Activity.java
@@ -2,7 +2,9 @@ package com.example.beta11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main2 );
+    }
+    public void sd(View view){
+        Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/beta11/Main3Activity.java
+++ app/src/main/java/com/example/beta11/Main3Activity.java
@@ -3,12 +3,22 @@ package com.example.beta11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
 
 public class Main3Activity extends AppCompatActivity {
-
+WebView we;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main3 );
+        we=(WebView)findViewById( R.id.tofes );
+
+    }
+    public void open (View view ){
+        we.getSettings().setJavaScriptEnabled( true );
+        String url="https://tofes101.co.il/fill-form-101/";
+        we.loadUrl( url );
+
     }
 }
